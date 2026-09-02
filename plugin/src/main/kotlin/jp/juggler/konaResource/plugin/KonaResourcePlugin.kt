38d58291a1ef672cfced0d409951b56a217c13a9
tateisu <tateisu@gmail.com>
2026-09-02 13:58:56 +0900

    Support cross compiled resource objects

--- plugin/src/main/kotlin/jp/juggler/konaResource/plugin/KonaResourcePlugin.kt
+++ plugin/src/main/kotlin/jp/juggler/konaResource/plugin/KonaResourcePlugin.kt
@@ -7,9 +7,11 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.file.Directory
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
+import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFiles
@@ -20,7 +22,19 @@ import org.gradle.api.tasks.TaskAction
 import org.gradle.process.ExecOperations
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import java.util.Locale
 import javax.inject.Inject
+
+private fun compilerForTarget(targetName: String): String = when (targetName.lowercase(Locale.ROOT)) {
+    "linuxarm64" -> "aarch64-linux-gnu-gcc"
+    "mingwx64" -> if (System.getProperty("os.name").lowercase(Locale.ROOT).contains("windows")) {
+        "cc"
+    } else {
+        "x86_64-w64-mingw32-gcc"
+    }
+
+    else -> "cc"
+}
 
 @Suppress("unused")
 class KonaResourcePlugin : Plugin<Project> {
@@ -29,6 +43,7 @@ class KonaResourcePlugin : Plugin<Project> {
         val generate = project.tasks.register("generateKonaResource", GenerateKonaResourceTask::class.java)
         generate.configure { task ->
             task.outputDirectory.set(project.layout.buildDirectory.dir("generated/konaResource"))
+            task.compiler.convention("cc")
         }
         project.afterEvaluate {
             generate.configure { task ->
@@ -56,20 +71,51 @@ class KonaResourcePlugin : Plugin<Project> {
         }
         project.pluginManager.withPlugin("org.jetbrains.kotlin.multiplatform") {
             project.afterEvaluate {
-                project.extensions.getByType(KotlinMultiplatformExtension::class.java).targets
-                    .withType(KotlinNativeTarget::class.java).configureEach { target ->
-                        target.binaries.all { binary ->
-                            val linkerOptions: Array<String> = extension.modules.map { module ->
-                                val safeName = module.first.replace(Regex("[^A-Za-z0-9_]"), "_")
-                                generate.get().outputDirectory.get().file("$safeName.o").asFile.absolutePath
-                            }.toTypedArray()
-                            binary.linkerOpts(*linkerOptions)
-                            binary.linkTaskProvider.configure { it.dependsOn(generate) }
-                        }
+                fun configureGenerateTask(
+                    task: GenerateKonaResourceTask,
+                    outputDirectory: Provider<Directory>,
+                ) {
+                    task.outputDirectory.set(outputDirectory)
+                    task.resourceDirectories.from(
+                        extension.modules.map { (_, rawDirectory) -> project.file(rawDirectory) },
+                    )
+                    task.moduleNames.set(extension.modules.map { it.first })
+                    task.lz4CompressionLevel.set(extension.lz4CompressionLevel)
+                    task.lz4BlockSizeID.set(extension.lz4BlockSizeID)
+                    task.lz4BlockMode.set(extension.lz4BlockMode)
+                    task.lz4ContentSizeFlag.set(extension.lz4ContentSizeFlag)
+                    task.lz4ContentChecksumFlag.set(extension.lz4ContentChecksumFlag)
+                    task.lz4blockChecksumFlag.set(extension.lz4blockChecksumFlag)
+                    task.lz4AutoFlush.set(extension.lz4AutoFlush)
+                    task.lz4FavorDecSpeed.set(extension.lz4FavorDecSpeed)
+                }
+
+                val kotlin = project.extensions.getByType(KotlinMultiplatformExtension::class.java)
+                kotlin.targets.withType(KotlinNativeTarget::class.java).configureEach { target ->
+                    val targetName = target.name.replaceFirstChar { it.uppercase() }
+                    val targetGenerate = project.tasks.register(
+                        "generateKonaResource$targetName",
+                        GenerateKonaResourceTask::class.java,
+                    )
+                    targetGenerate.configure { task ->
+                        configureGenerateTask(
+                            task,
+                            project.layout.buildDirectory.dir("generated/konaResource/${target.name}"),
+                        )
+                        task.compiler.set(compilerForTarget(target.name))
                     }
+                    target.binaries.all { binary ->
+                        val linkerOptions: Array<String> = extension.modules.map { module ->
+                            val safeName = module.first.replace(Regex("[^A-Za-z0-9_]"), "_")
+                            targetGenerate.get().outputDirectory.get().file("$safeName.o").asFile.absolutePath
+                        }.toTypedArray()
+                        binary.linkerOpts(*linkerOptions)
+                        binary.linkTaskProvider.configure { it.dependsOn(targetGenerate) }
+                    }
+                }
             }
-        }
     }
+}
 }
 
 @CacheableTask
@@ -106,6 +152,9 @@ abstract class GenerateKonaResourceTask @Inject constructor(
 
     @get:Input
     abstract val lz4FavorDecSpeed: Property<Boolean>
+
+    @get:Input
+    abstract val compiler: Property<String>
 
     @get:OutputDirectory
     abstract val outputDirectory: DirectoryProperty
@@ -158,7 +207,7 @@ abstract class GenerateKonaResourceTask @Inject constructor(
                 Action { spec ->
                     spec.workingDir(output)
                     spec.commandLine(
-                        "cc",
+                        compiler.get(),
                         "-c",
                         "-x",
                         "assembler-with-cpp",
